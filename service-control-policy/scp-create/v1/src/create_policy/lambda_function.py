c2b9336ae26eaf51fd5c84aa973103e1b64ca73a
Eamonn Faherty <eamonnf@amazon.co.uk>
2019-11-22 15:26:33 +0000

    cleaning up

--- service-control-policy/scp-create/v1/src/create_policy/lambda_function.py
+++ service-control-policy/scp-create/v1/src/create_policy/lambda_function.py
@@ -1,12 +1,5 @@
-# Any code, applications, scripts, templates, proofs of concept, documentation
-# and other items provided by AWS under this SOW are "AWS Content,"" as defined
-# in the Agreement, and are provided for illustration purposes only. All such
-# AWS Content is provided solely at the option of AWS, and is subject to the
-# terms of the Addendum and the Agreement. Customer is solely responsible for
-# using, deploying, testing, and supporting any code and applications provided
-# by AWS under this SOW.
-#
-# (c) 2019 Amazon Web Services
+# Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
 
 import logging
 import json
