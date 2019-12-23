eff9ec1d306bd5319f744d246d95382088c0750a
smuralee <suraj@surajmuraleedharan.com>
2019-12-23 17:35:12 -0600

    Upgraded the serialize-javascript version to 2.1.2 for XSS vulnerability fix

---
+++ service-catalog-tools-dashboard/static-web-app/v2/src/app/list-launches/list-launches.component.spec.ts
@@ -0,0 +1,25 @@
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {ListLaunchesComponent} from './list-launches.component';
+
+describe('ListLaunchesComponent', () => {
+    let component: ListLaunchesComponent;
+    let fixture: ComponentFixture<ListLaunchesComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ListLaunchesComponent]
+        })
+            .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ListLaunchesComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
